6c3b3313234fe1dac1451939caeaf7223c877c02
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-11-08 23:42:10 +0530

    s

--- Leet2914.java
+++ Leet2914.java
@@ -1,12 +1,14 @@
-import java.util.*;
-public class Leet2914 {
-    public int minChanges(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length() - 1; i += 2) {
-            if (s.charAt(i) != s.charAt(i + 1)) {
-                count++;
+class Leet2914 {
+    public int[] findErrorNums(int[] nums) {
+        int[] arr = new int[2];
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]){
+                arr[0] = nums[i];
+                arr[1] = nums[i]+1;
+                break;
             }
         }
-        return count;
+        return arr;
     }
 }
